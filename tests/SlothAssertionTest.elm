0428dbfef1f14143bdb4f577ee34a1cead56729e
Po-Ying Chen <poying.me@gmail.com>
2015-12-26 12:35:43 +0800

    test

--- tests/SlothAssertionTest.elm
+++ tests/SlothAssertionTest.elm
@@ -9,14 +9,10 @@ tests =
   start
     `describe` "Sloth.Assertion"
       `describe` "shouldBe"
-        `it` "should fail" =>
-          (1 `shouldBe` 2)
-        `it` "should success" =>
+        `it` "should pass" =>
           (1 `shouldBe` 1)
         `end` 1
       `describe` "shouldNotBe"
-        `it` "should fail" =>
-          (1 `shouldNotBe` 1)
-        `it` "should success" =>
+        `it` "should pass" =>
           (1 `shouldNotBe` 2)
         `end` 1
